4d9be9e67edfffec3bb33eb0908f7a64b5e28f54
Alex-zReeZ <alexbeck220@gmail.com>
2023-11-28 13:59:59 +0100

    add : color next to the grade, based on the grade

--- src/main.js
+++ src/main.js
@@ -1,25 +1,39 @@
+
 const button = document.querySelector('#button-1');
 const inputValue = document.getElementById('sem1')
 let allGrades = document.querySelector('#allGrades')
 
-//all the color for the circles
-const redSVG = document.querySelector('#')
+//all the color for the circles before the grade
+const redSVG = document.querySelector('#redSVG').content.querySelector("svg")
+const orangeSVG = document.querySelector('#orangeSVG').content.querySelector("svg")
+const greenSVG = document.querySelector('#greenSVG').content.querySelector("svg")
 
 
-
+// add grade to the list
 button.addEventListener('click', () => {
+    //take the number in input
     const newValue = document.createElement('span');
     newValue.className = "inline-flex items-center gap-x-1.5 rounded-md px-2 py-2 text-sm font-medium text-gray-900 ring-1 ring-inset ring-gray-200"
-    console.log(newValue)
 
-    let circle = document.createElement('svg')
+    //what color goes before the grade
+    let circle = document.createElement('svg');
 
+    if (inputValue.value < 4) {
+        circle = redSVG.cloneNode(true)
+    }
+    else if (inputValue.value > 4) {
+        circle = greenSVG.cloneNode(true)
+    }
+    else if (inputValue.value === 4 + ""){
+        circle = orangeSVG.cloneNode(true)
+    }
 
-
-
+    newValue.appendChild(circle)
     newValue.appendChild(document.createTextNode(inputValue.value))
+    console.log(circle)
 
-    if (allGrades) {
+
+    if (allGrades && inputValue.value >= 1 && inputValue.value <= 6) {
         allGrades.appendChild(newValue)
     }
 })
