66a53559ac33710445408707e8e73ea619213c9d
Alex-zReeZ <alexbeck220@gmail.com>
2023-12-04 11:24:40 +0100

    add : code rearrangement,  and average calculator started

--- src/main.js
+++ src/main.js
@@ -1,18 +1,13 @@
 const button = document.querySelector('#button-1');
 const buttonSemester = document.querySelector('#button-semester');
-
 const inputValue = document.getElementById('sem1')
+
 let allGrades = document.querySelector('#allGrades')
-
-
 let semesterNumber = 1;
 
 
 
-
 /*****************************add a grades to the semester**********************/
-
-// add grade to the list
 button.addEventListener('click', () => {
 
     //all the color for the circles before the grade
@@ -23,7 +18,7 @@ button.addEventListener('click', () => {
 
     //take the number in input
     const newValue = document.createElement('span');
-    newValue.className = 
+    newValue.className =
     "inline-flex items-center gap-x-1.5 rounded-md px-2 py-2 text-sm font-medium text-gray-900 ring-1 ring-inset ring-gray-200"
 
     //what color goes before the grade
@@ -31,12 +26,15 @@ button.addEventListener('click', () => {
 
     if (inputValue.value < 4) {
         circle = redSVG.cloneNode(true)
+        grades.push(inputValue.value)
     }
     else if (inputValue.value > 4) {
         circle = greenSVG.cloneNode(true)
+        grades.push(inputValue.value)
     }
     else if (inputValue.value === 4 + ""){
         circle = orangeSVG.cloneNode(true)
+        grades.push(inputValue.value)
     }
 
     newValue.appendChild(circle)
@@ -47,44 +45,62 @@ button.addEventListener('click', () => {
     }
 })
 
-//average calculator
+/**************************** calculate the average ******************************/
+document.addEventListener('DOMContentLoaded', function () {
+    const divElements = document.querySelectorAll('#allGrades');
 
+    let somme = 0;
 
-//border color change when input is focused
-button.addEventListener('focus', () => {
-    const border = document.querySelector('#input');
-    button.sty
-})
+    divElements.forEach(div => {
+
+        const gradeValue = parseFloat(div.getAttribute('data-valeur')) || 0;
+
+        somme += gradeValue;
+    });
+});
+
+/*************************** Hover when focused ********************************/
+button.addEventListener('focus', (event) => {
+    const buttonClassList = button.classList;
+    const classSVG = button.querySelector("#uploadSVG");
+
+    buttonClassList.remove("ring-gray-300");
+
+    classSVG.classList.remove("mx-auto h-8 w-8 text-gray-400");
+    classSVG.classList.add("mx-auto h-8 w-8 text-blue-400");
+  });
+button.addEventListener("blur", (event) => {
+    const buttonClass = button.classList;
+    const svgClassList = button.querySelector("#uploadSVG");
+
+    buttonClass.remove("ring-blue-300");
+    buttonClass.add("ring-gray-300");
+
+    svgClassList.classList.remove("text-blue-400");
+    svgClassList.classList.add("text-gray-400");
+  });
+
 
 /******************************add a semester **********************************/
-
 buttonSemester.addEventListener('click', () => {
-    //create the additional semester div for look
-    const newSemester = document.createElement('div')
-    newSemester.className = "px-4 py-6 sm:grid sm:grid-cols-5 sm:gap-4 sm:px-0"
+    const newSemester = document.querySelector('#semester-template').content.cloneNode(true);
+    const semesterPlacement = document.querySelector('#semesterClass');
+    semesterPlacement.appendChild(newSemester)
+
+    const Semester = semesterPlacement.lastElementChild
+
+    //print the semester count on the page
+    Semester.querySelector("dt").textContent = "Semester " + semesterNumber;
 
     //add 1 to the semester count
     if (semesterNumber <= 7) {
-    //add 1 on the number of semester
+        //add 1 on the number of semester
         semesterNumber++;
-        console.log(semesterNumber)
     } else if (semesterNumber > 7 ) {
         //remove button if 8 semester
         buttonSemester.remove();
-        className.remove('px-4 py-6 sm:gap-4 sm:px-0')
-        className.remove('px-4 py-6 sm:gap-4 sm:px-0')
     }
-    
-    //print the semester count on the page
-    newSemester.textContent = "Semester " + semesterNumber;
-    const semesterPlacement = document.querySelector('#semesterClass')
 
-    //input option
-    const inputCase = document.createElement('div');
-    
-    newSemester.appendChild = inputCase;
-
-    semesterPlacement.appendChild(newSemester)
 })
 
 
